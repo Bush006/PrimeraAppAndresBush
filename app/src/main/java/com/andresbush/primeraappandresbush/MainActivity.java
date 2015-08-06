9e242def32c91f89bdc34a4456d382ffeecba752
Bush <andres.bush@hotmail.com>
2015-08-06 11:06:53 -0500

    prueba tab

--- app/src/main/java/com/andresbush/primeraappandresbush/MainActivity.java
+++ app/src/main/java/com/andresbush/primeraappandresbush/MainActivity.java
@@ -1,5 +1,6 @@
 package com.andresbush.primeraappandresbush;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,14 +10,18 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+    MediaPlayer mySound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySound = MediaPlayer.create(this, R.raw.beat);
+
     }
 
     public void Metodo(View view) {
-        Toast.makeText(this, "Hola mundo", Toast.LENGTH_LONG).show();
-
+        //Toast.makeText(this, "Hola mundo", Toast.LENGTH_LONG).show();
+        mySound.start();
     }
 }
